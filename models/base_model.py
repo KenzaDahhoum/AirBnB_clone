4647cbaa595eb8efcfa71f19bd4796aaad607fef
Yassine <yassinemoussafir31@gmail.com>
2024-03-09 06:29:48 -0800

    base model pycode

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""
+This module defines the BaseModel class which serves as the base class
+for all models in our application.
+"""
 
 import uuid
 from datetime import datetime
-
 from . import storage
 
+
 class BaseModel:
+    """
+    Defines all common attributes/methods for other classes.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel.
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -22,16 +33,24 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
+        """
+        Provides a string representation of the instance.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
         storage.save()
+
     def to_dict(self):
+        """
+        Creates a dictionary representation of the instance.
+        """
         instance_dict = self.__dict__.copy()
         instance_dict['created_at'] = self.created_at.isoformat()
         instance_dict['updated_at'] = self.updated_at.isoformat()
         instance_dict['__class__'] = self.__class__.__name__
-        return(instance_dict)
-    
-
-
+        return instance_dict
